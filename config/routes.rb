9009e186bb3008813c7e207abf45c1c0b8d2ef73
南辰憲 <tatsunori.minami@gmail.com>
2018-02-19 18:44:10 +0900

    Edit routes.rb
    インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    root 'messages#index'
+  root 'messages#index'
 
   get "groups/new" => "messages#new"
   get "users/id/edit" => "messages#edit"

5e840ecb77e02cac21127e8cd89b4cf4d10a6d77
南辰憲 <tatsunori.minami@gmail.com>
2018-02-21 13:37:51 +0900

    Edit admin_user
    デバイスモデルのユーザー管理対象をアドミンユーザーから変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'messages#index'
-
-  get "groups/new" => "messages#new"
-  get "users/id/edit" => "messages#edit"
+  get "messages" => "messages#index"
+  resource :user, only: [:edit, :update, :destroy]
 
 end

8b98a5fbf85f785ba2d3bff5e38a49f850e80f1f
南辰憲 <tatsunori.minami@gmail.com>
2018-02-22 17:56:51 +0900

    Edit routes.eb
    rootパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'messages#index'
+  root 'groups#index'
   resource :user, only: [:edit, :update]
   resource :group, only: [:new, :create, :edit, :update] do
     resource :messages, only: [:index, :create]

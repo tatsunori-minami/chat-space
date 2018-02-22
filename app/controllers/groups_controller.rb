2a9378f0b96cb13fd01c860e234a4612fb9c8583
南辰憲 <tatsunori.minami@gmail.com>
2018-02-22 17:52:45 +0900

    Edit groups_cntroller.rb
    アクション追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,11 +21,20 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name,{user_ids: []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end

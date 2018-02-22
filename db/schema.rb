a320dddcb6276ac23df3947ad95079234a75e4f6
南辰憲 <tatsunori.minami@gmail.com>
2018-02-22 11:09:17 +0900

    Edit admin
    アドミンユーザー関連の削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221050939) do
+ActiveRecord::Schema.define(version: 20180222020420) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180221050939) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end

f440e18b390721f0c6b3e100bb87fecacb50c7b3
南辰憲 <tatsunori.minami@gmail.com>
2018-02-23 15:00:33 +0900

    edit image_uploader.rb
    MiniMagickコメントアウトを削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file

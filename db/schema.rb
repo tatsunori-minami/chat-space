21dc36cf71858b7e8f5ee864212b94688c49889e
南辰憲 <tatsunori.minami@gmail.com>
2018-02-27 11:18:17 +0900

    Edit schema.rb
    マイグレーションをドロップ

--- db/schema.rb
+++ db/schema.rb
@@ -38,17 +38,6 @@ ActiveRecord::Schema.define(version: 20180223091529) do
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
-  create_table "messeages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "content"
-    t.string   "image"
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_messeages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_messeages_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
@@ -71,6 +60,4 @@ ActiveRecord::Schema.define(version: 20180223091529) do
   add_foreign_key "group_users", "users"
   add_foreign_key "messages", "groups"
   add_foreign_key "messages", "users"
-  add_foreign_key "messeages", "groups"
-  add_foreign_key "messeages", "users"
 end
